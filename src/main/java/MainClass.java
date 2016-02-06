336220a2257b739904c6d560b678e1f78a3194cc
Solidworks <pantherbotics@gmail.com>
2016-02-05 20:02:13 -0800

    Eh Stuffs

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,9 +1,11 @@
 import com.github.sarxos.webcam.Webcam;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 /**
  * Created by robotics on 2/3/2016.
@@ -16,5 +18,19 @@ public class MainClass {
         BufferedImage image = webcam.getImage();
         ImageIO.write(image, "JPG", new File("test.jpg"));
 
+        float[] hsb = new float[3];
+
+        for (int x = 0; x < image.getWidth(); x++) {
+            for (int y = 0; y < image.getHeight(); y++) {
+                int rgbData = image.getRGB(x, y);
+                int red = (rgbData & 0x00FF0000) >> 16;
+                int green = (rgbData & 0x0000FF00) >> 8;
+                int blue = (rgbData & 0x000000FF);
+                Color.RGBtoHSB(red, green, blue, hsb);
+
+                System.out.println((rgbData & 0x00FFFFFF) + " | " + red + " | " + green + " | " + blue + " | " + Arrays.toString(hsb));
+            }
+        }
     }
+
 }
